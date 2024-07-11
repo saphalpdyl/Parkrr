2971643fcc252a4b83b3e1f977eb0f1996ac96a8
saphalpdyl <saphalpdyl@gmail.com>
2024-07-11 19:04:57 +0545

    add: border to navbar

--- client/src/routes/editor/components/EditorSidebar.tsx
+++ client/src/routes/editor/components/EditorSidebar.tsx
@@ -2,7 +2,7 @@ type EditorSidebarProps = {};
 
 const EditorSidebar = ({}: EditorSidebarProps) => {
   return (
-    <div className="fixed left-0 z-20 h-1/2 w-16 rounded-e-xl bg-gray-100 shadow-md transition-all hover:w-24"></div>
+    <div className="fixed left-0 z-20 h-1/2 w-16 rounded-e-xl bg-gray-100 shadow-md transition-all hover:w-24 border-e-2 border-y-2 border-slate-600"></div>
   );
 };
 export default EditorSidebar;
