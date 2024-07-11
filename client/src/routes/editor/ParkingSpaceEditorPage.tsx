e5d4f96d524b3eb4ff6730edc04715f3d7ab9786
saphalpdyl <saphalpdyl@gmail.com>
2024-07-11 18:59:43 +0545

    refactor: conveted navbar and grid to components

--- client/src/routes/editor/ParkingSpaceEditorPage.tsx
+++ client/src/routes/editor/ParkingSpaceEditorPage.tsx
@@ -13,6 +13,8 @@ import {
 } from "@dnd-kit/modifiers";
 import type { Position, Item } from "./types";
 import DraggableItem from "./components/DraggableItem";
+import EditorSidebar from "./components/EditorSidebar";
+import BackgroundGrid from "./components/BackgroundGrid";
 
 const DragAndDropPage = () => {
   const [items] = useState<Item[]>([
@@ -56,27 +58,8 @@ const DragAndDropPage = () => {
 
   return (
     <div className="relative flex h-screen w-screen items-center overflow-hidden">
-      <div className="fixed left-0 z-20 h-1/2 w-16 rounded-e-xl bg-gray-100 shadow-md transition-all hover:w-24"></div>
-      <div className="pointer-events-none absolute left-0 top-0 z-10 h-screen w-screen bg-[#f0f0f0]">
-        <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
-          <defs>
-            <pattern
-              id="grid"
-              width={gridSize}
-              height={gridSize}
-              patternUnits="userSpaceOnUse"
-            >
-              <path
-                d={`M ${gridSize} 0 L 0 0 0 ${gridSize}`}
-                fill="none"
-                stroke="#6662"
-                strokeWidth="1"
-              />
-            </pattern>
-          </defs>
-          <rect width="100%" height="100%" fill="url(#grid)" />
-        </svg>
-      </div>
+      <EditorSidebar />
+      <BackgroundGrid gridSize={gridSize} />
       <DndContext
         onDragStart={handleDragStart}
         onDragEnd={handleDragEnd}
---
+++ client/src/routes/editor/components/EditorSidebar.tsx
@@ -0,0 +1,8 @@
+type EditorSidebarProps = {};
+
+const EditorSidebar = ({}: EditorSidebarProps) => {
+  return (
+    <div className="fixed left-0 z-20 h-1/2 w-16 rounded-e-xl bg-gray-100 shadow-md transition-all hover:w-24"></div>
+  );
+};
+export default EditorSidebar;
