5e8e4d12ea9d783a8b2c195bdfedcfbee9a68269
saphalpdyl <saphalpdyl@gmail.com>
2024-07-14 00:20:23 +0545

    add: bounce in sidebar item if clicked

--- client/src/routes/editor/components/EditorSidebar/EditorSidebarButton.tsx
+++ client/src/routes/editor/components/EditorSidebar/EditorSidebarButton.tsx
@@ -19,7 +19,7 @@ function EditorSidebarButton({
     <div className="flex flex-col gap-1 items-center">
       <div 
         onClick={onClick}
-        className={twMerge("p-2 rounded-lg bg-blue-500 cursor-pointer hover:bg-blue-600", className)}>
+        className={twMerge("p-2 rounded-lg bg-blue-500 cursor-pointer hover:bg-blue-600 active:-translate-y-2 transition-all translate-y-0", className)}>
         <Icon className="text-white" />
       </div>
       <span className="font-light text-xs text-center">{ title }</span>
