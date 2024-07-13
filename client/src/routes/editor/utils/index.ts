e0f886c7640b8f026f4c38b280ab5c7370af06f1
saphalpdyl <saphalpdyl@gmail.com>
2024-07-13 23:26:58 +0545

    fix: centered origin not really looking centered

--- client/src/routes/editor/utils/index.ts
+++ client/src/routes/editor/utils/index.ts
@@ -2,7 +2,9 @@ import { SIZE_FACTOR } from "../constants";
 
 export const calculateCenterPosition = (height: number, width: number) => {
   // Calculate center position
-  const centerX = Math.round((width / 2) / SIZE_FACTOR) * SIZE_FACTOR;
+  // - SIZE_FACTOR * 3 to compensate for the DndContext being 
+  // pushed to the right by almost 3 SIZE_FACTOR worth of space
+  const centerX = Math.round((width / 2 - SIZE_FACTOR * 3) / SIZE_FACTOR) * SIZE_FACTOR;
   const centerZ = Math.round((height / 2) / SIZE_FACTOR) * SIZE_FACTOR;
 
   return { x: centerX, z: centerZ}  
