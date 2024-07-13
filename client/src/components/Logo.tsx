b5d683d1165a461ce86e604929f7158146c17ba9
saphalpdyl <saphalpdyl@gmail.com>
2024-07-13 14:10:54 +0545

    fix: logo cannot be dragged out in editor space

--- client/src/components/Logo.tsx
+++ client/src/components/Logo.tsx
@@ -2,6 +2,6 @@ import applicationLogo from "/logo_main_t.png";
 
 export default function Logo() {
   return (
-    <img className="z-40 h-10" src={applicationLogo} alt="Parkrr Logo" />
+    <img className="z-40 h-10 pointer-events-none" src={applicationLogo} alt="Parkrr Logo" />
   )
 }
