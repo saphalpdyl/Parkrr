976074524db00066cc9ac25b4fb4c35fbc6c6acc
saphalpdyl <saphalpdyl@gmail.com>
2024-07-16 19:34:55 +0545

    refactor: seperated logic to seperate controllers

---
+++ server/controllers/auth/signUpController.ts
@@ -0,0 +1,25 @@
+import { Context } from "hono";
+import { AuthService } from "../../services/authService";
+import { AuthError } from "../../errors/error";
+
+export default async function signUpController(c : Context) {
+  const body: AuthService.UserRegisterData = await c.req.json();
+  
+  try {
+    const { firstName, lastName, username } = await AuthService.createUser(body);
+    const jwtToken = await AuthService.loginUser(body.username, body.username);
+    
+    return c.json({ firstName, lastName, username, token: jwtToken });
+  } catch(e: any) {
+    if ( e instanceof AuthError ) {
+      c.status(400);
+      return c.json(e.toJSON())
+    }
+  
+    c.status(500);
+    c.json({
+      message: "An unknown error occured",
+    });
+    return;
+  }
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,35 +1,12 @@
 import { Hono } from 'hono'
 
 import mongoose from "mongoose";
-import { AuthService } from '../services/authService';
-import { AuthError } from '../errors/error';
+import signUpController from '../controllers/auth/signUpController';
 
 const app = new Hono();
 
 mongoose.connect(process.env.MONGODB_URL!);
 
-app.post("/auth/signup/", async c => {
-  const body: AuthService.UserRegisterData = await c.req.json();
-  
-  try {
-    const { firstName, lastName, username } = await AuthService.createUser(body);
-    const jwtToken = await AuthService.loginUser(body.username, body.username);
-    
-    return c.json({ firstName, lastName, username, token: jwtToken });
-  } catch(e: any) {
-    console.log(typeof e, e instanceof AuthError);
-    // console.log(e);
-    if ( e instanceof AuthError ) {
-      c.status(400);
-      return c.json(e.toJSON())
-    }
-
-    c.status(500);
-    c.json({
-      message: "An unknown error occured",
-    });
-    return;
-  }
-})
+app.post("/auth/signup/", signUpController);
 
 export default app
