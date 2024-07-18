2e6a0d60859a94c8fa03ad1057763265351d9e46
saphalpdyl <saphalpdyl@gmail.com>
2024-07-18 22:11:03 +0545

    add: better managed the logic by implementing DRY

--- client/src/hooks/useAuth.ts
+++ client/src/hooks/useAuth.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import useAuthStore from "../stores/authState";
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
 export interface SignUpData {
   firstName: string;
@@ -25,37 +25,35 @@ export default function useAuth() {
     }
   }, [token]);
 
-  const login = async (username: string, password: string) => {
-    const payload = {
-      username,
-      password
-    };
-
-    const response = await axios.post("http://localhost:3000/api/v1/auth/login/", payload);
-    
+  const _handleUserResponse = (response: AxiosResponse<any,any>) => {
     if ( response.status !== 200 ) {
       // Do Something about the error
       clearUserAndToken();
+      setError(response.data.message);
       return;
     }
 
     const {token, user } = response.data;
     setUserAndToken(user!, token!);
     localStorage.setItem("token", token);
+    setLoading(false);
+  }
+  
+  const login = async (username: string, password: string) => {
+    setLoading(true);
+    const payload = {
+      username,
+      password
+    };
+    
+    const response = await axios.post("http://localhost:3000/api/v1/auth/login/", payload);
+    _handleUserResponse(response);    
   }
   
   const signUp = async (payload: SignUpData) => {
+    setLoading(true);
     const response = await axios.post("http://localhost:3000/api/v1/auth/signup/", payload);
-    
-    if ( response.status !== 200 ) {
-      // Do Something about the error
-      clearUserAndToken();
-      return;
-    }
-    
-    const {token, user} = response.data;
-    setUserAndToken(user!, token!);
-    localStorage.setItem("token", token);
+    _handleUserResponse(response); 
   }
   
   return {
