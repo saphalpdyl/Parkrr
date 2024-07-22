9826b3211ab69a67a2d29b7403b02bef5d1ff276
saphalpdyl <saphalpdyl@gmail.com>
2024-07-22 20:16:45 +0545

    fix: non clickable buttons in editor name bar

--- client/src/routes/editor/components/EditorNameBar.tsx
+++ client/src/routes/editor/components/EditorNameBar.tsx
@@ -12,6 +12,10 @@ function EditorNameBar() {
   }, [currentEditor])
   
   if ( !currentEditor ) return null;
+
+  function handleRenameEditor() {
+
+  }
   
   return (
     <div className="absolute top-0 left-1/2 -translate-x-1/2 w-60 focus-within:w-80 h-10 bg-white rounded-b-md shadow-md flex justify-center items-center gap-3 z-50 transition-all overflow-hidden">
@@ -21,9 +25,9 @@ function EditorNameBar() {
         type="text" 
         className="peer bg-transparent outline-none text-blue-500 font-semibold text-lg text-center max-w-48 w-auto"/>
       <span className="font-bold text-gray-400">.edit</span>
-      <div className="flex gap-1 text-slate-800 peer-focus:opacity-100 opacity-0 peer-focus:scale-100 scale-0 transition-all cursor-pointer">
+      <div className="flex gap-1 text-slate-800 peer-focus:opacity-100 active:scale-100 opacity-0 peer-focus:scale-100 scale-0 delay-0 peer-focus:delay-200 active:delay-200 transition-all cursor-pointer">
         <Check className="hover:text-blue-500 transition-all" />
-        <X onClick={() => setEditorName(currentEditor?.name ?? "Untitled")} className="hover:text-red-500 transition-all" />
+        <X onClick={() => setEditorName(currentEditor.name || null)} className="hover:text-red-500 transition-all" />
       </div>
     </div>
   );
