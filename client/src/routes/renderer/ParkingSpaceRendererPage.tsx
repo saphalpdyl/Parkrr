d07da1b20ba59e898a9852554717527c4ac492c4
saphalpdyl <saphalpdyl@gmail.com>
2024-07-26 01:19:27 +0545

    fix: cursor invisible in renderer

--- client/src/routes/renderer/ParkingSpaceRendererPage.tsx
+++ client/src/routes/renderer/ParkingSpaceRendererPage.tsx
@@ -38,7 +38,7 @@ function ParkingSpaceRendererPage() {
 
 
   return (
-    <div className="h-screen w-screen bg-gray-100 cursor-none">
+    <div className="h-screen w-screen bg-gray-100">
       <StatusBar />
       <ToolBar />
 
