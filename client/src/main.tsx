076ef3f5384fc8bb25c7f6ee6c5f2592cffed381
saphalpdyl <saphalpdyl@gmail.com>
2024-07-29 10:29:58 +0545

    setup: sonner

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { Toaster } from 'react-hot-toast'
+import { Toaster } from 'react-hot-toast';
+import { Toaster as SonnerToaster } from "@/components/ui/sonner.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Toaster />
+    <SonnerToaster />
     <App />
   </React.StrictMode>,
 )
