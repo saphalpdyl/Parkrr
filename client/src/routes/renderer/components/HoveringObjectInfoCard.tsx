b95a6dd0c608e6b7098aa20d75df8aaf97271d3c
saphalpdyl <saphalpdyl@gmail.com>
2024-07-31 08:25:55 +0545

    add: hovering info skeleton when not hovering anything

--- client/src/routes/renderer/components/HoveringObjectInfoCard.tsx
+++ client/src/routes/renderer/components/HoveringObjectInfoCard.tsx
@@ -1,6 +1,7 @@
 import useHover from "@/routes/renderer/hooks/useHover.ts";
 import calculateAndReturnNearestEntrance from "@/routes/renderer/helpers/calculateAndReturnNearestEntrance.ts";
 import useRenderer from "@/hooks/useRenderer.ts";
+import { Skeleton } from "@/components/ui/skeleton";
 
 function HoveringObjectInfoCard() {
   const { hovering } = useHover();
@@ -10,13 +11,13 @@ function HoveringObjectInfoCard() {
 
   return (
     <div className={`
-      absolute z-40 top-28 left-3 w-52 bg-white rounded-lg shadow-lg p-3
-      transition-opacity
-      ${hovering ? "opacity-100" : "opacity-0 delay-75"}
+      absolute z-10 top-28 left-3 w-52 bg-white rounded-lg shadow-lg p-3
+      transition-all
       flex flex-col
+      ${hovering ? "opacity-100" : "opacity-0 delay-75"}
       `}>
       {
-        hovering && (
+        hovering != null ? (
           <>
             <span className="text-xs text-gray-500 font-semibold">Currently hovering</span>
             <span className="text-sm text-gray-700 font-bold capitalize mt-1">{ "type" in hovering ? `${hovering.type} Parking Space` : "" }</span>
@@ -27,6 +28,14 @@ function HoveringObjectInfoCard() {
 
               )
             }
+
+          </>
+        ) : (
+          <>
+            <Skeleton className="h-[10px] w-1/2" />
+            <Skeleton className="h-[13px] w-2/3 mt-2" />
+            <Skeleton className="h-[7px] w-2/3 mt-2" />
+            <Skeleton className="h-[7px] w-full mt-4" />
 
           </>
         )
